3960d9aba2b380f4b2b549bdc00fb81d4ede91f6
Legenseed <agungsurya0042@gmail.com>
2024-12-30 15:14:43 +0800

    adding text for toggle button

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const toggleThemeButton = document.querySelector('.toggle-theme');
 const body = document.body;
 
 const tema = localStorage.getItem('tema');
-
 if (tema === 'dark-mode') {
   body.classList.add('dark-mode');
 }
 
 toggleThemeButton.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
+  toggleThemeButton.textContent = body.classList.contains('dark-mode') ? 'Active!' : 'Toggle Theme';
   localStorage.setItem('tema', body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode');
 });
